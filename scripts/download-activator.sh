4962bb16da4c20b183667ba646fcc77ceb1ec80a
Jordan Fernando <fernandojordan.92@gmail.com>
2015-11-23 14:50:46 +0700

    Change activator script to download activator inside of judgels

--- scripts/download-activator.sh
+++ scripts/download-activator.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
+JUDGELS_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+ACTIVATOR_DIR="$JUDGELS_BASE_DIR/activator"
+
 echo "Downloading Typesafe Activator..."
 
-mkdir -p ~/activator
-pushd ~/activator > /dev/null
+mkdir -p $ACTIVATOR_DIR 
+pushd $ACTIVATOR_DIR > /dev/null
 wget -q http://downloads.typesafe.com/typesafe-activator/1.3.2/typesafe-activator-1.3.2-minimal.zip
 unzip typesafe-activator-1.3.2-minimal.zip
 mv activator-1.3.2-minimal/* .
 rm -rf activator-1.3.2-minimal *.zip
 popd > /dev/null
 
-echo "Activator successfully downloaded. Export the executable in PATH by adding this line to your .bashrc:"
-echo "    export PATH=\$PATH:~/activator"
+echo "export PATH=\$PATH:$ACTIVATOR_DIR" >> ~/.profile
+echo "This script add PATH to your ~/.profile."
+echo "Restart terminal to run activator from terminal."
--- scripts/download-checkstyle.sh
+++ scripts/download-checkstyle.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 JUDGELS_BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 
 echo "Downloading Checkstyle..."
